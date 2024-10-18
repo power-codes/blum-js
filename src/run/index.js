0f8bac79a79472dd61b15536499af6061953b8b8
power-codes <98588963+power-codes@users.noreply.github.com>
2024-10-19 01:39:04 +0330

    Add files via upload

---
+++ src/run/index.js
@@ -0,0 +1,213 @@
+import colors from "colors";
+import dayjs from "dayjs";
+import datetimeHelper from "../helpers/datetime.js";
+import delayHelper from "../helpers/delay.js";
+import fileHelper from "../helpers/file.js";
+import generatorHelper from "../helpers/generator.js";
+import authService from "../services/auth.js";
+import dailyService from "../services/daily.js";
+import farmingClass from "../services/farming.js";
+import gameService from "../services/game.js";
+import inviteClass from "../services/invite.js";
+import server from "../services/server.js";
+import taskService from "../services/task.js";
+import tribeService from "../services/tribe.js";
+import userService from "../services/user.js";
+
+const VERSION = "v0.1.7";
+const DELAY_ACC = 10;
+const MAX_RETRY_PROXY = 20;
+const MAX_RETRY_LOGIN = 20;
+const TIME_PLAY_GAME = [];
+
+const IS_SHOW_COUNTDOWN = true;
+const countdownList = [];
+
+let database = {};
+setInterval(async () => {
+  const data = await server.getData();
+  if (data) {
+    database = data;
+    server.checkVersion(VERSION, data);
+  }
+}, generatorHelper.randomInt(20, 40) * 60 * 1000);
+
+const run = async (user, index) => {
+  let countRetryProxy = 0;
+  let countRetryLogin = 0;
+  await delayHelper.delay((user.index - 1) * DELAY_ACC);
+  while (true) {
+    if (database?.ref) {
+      user.database = database;
+    }
+
+    countdownList[index].running = true;
+    let isProxyConnected = false;
+    while (!isProxyConnected) {
+      const ip = await user.http.checkProxyIP();
+      if (ip === -1) {
+        user.log.logError(
+          "Proxy error, check the proxy connection again, will try to connect again after 30 seconds"
+        );
+        countRetryProxy++;
+        if (countRetryProxy >= MAX_RETRY_PROXY) {
+          break;
+        } else {
+          await delayHelper.delay(30);
+        }
+      } else {
+        countRetryProxy = 0;
+        isProxyConnected = true;
+      }
+    }
+    try {
+      if (countRetryProxy >= MAX_RETRY_PROXY) {
+        const dataLog = `[No ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Proxy connection error -${user.proxy}`;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+
+      if (countRetryLogin >= MAX_RETRY_LOGIN) {
+        const dataLog = `[ON ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Login failed error too ${MAX_RETRY_LOGIN} `;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+    } catch (error) {
+        user.log.logError("Record error failed");
+    }
+
+    const login = await authService.handleLogin(user);
+    if (!login.status) {
+      countRetryLogin++;
+      await delayHelper.delay(60);
+      continue;
+    } else {
+      countRetryLogin = 0;
+    }
+
+    await dailyService.checkin(user);
+    await tribeService.handleTribe(user);
+    if (user.database?.skipHandleTask) {
+      user.log.log(
+        colors.yellow(
+          `Temporarily skip the mission due to server error (will automatically reopen when the server is stable)`
+        )
+      );
+    } else {
+      await taskService.handleTask(user);
+    }
+
+    await inviteClass.handleInvite(user);
+    let awaitTime = await farmingClass.handleFarming(
+      user,
+      login.profile?.farming
+    );
+    countdownList[index].time = (awaitTime + 1) * 60;
+    countdownList[index].created = dayjs().unix();
+    const minutesUntilNextGameStart = await gameService.handleGame(
+      user,
+      login.profile?.playPasses,
+      TIME_PLAY_GAME
+    );
+    if (minutesUntilNextGameStart !== -1) {
+      const offset = dayjs().unix() - countdownList[index].created;
+      const countdown = countdownList[index].time - offset;
+      if (minutesUntilNextGameStart * 60 < countdown) {
+        countdownList[index].time = (minutesUntilNextGameStart + 1) * 60;
+        countdownList[index].created = dayjs().unix();
+      }
+    }
+    countdownList[index].running = false;
+    await delayHelper.delay((awaitTime + 1) * 60);
+  }
+};
+
+console.log(
+  colors.yellow.bold(
+    `=============  PowerCodes  =============`
+  )
+);
+console.log(
+  "Any act of selling tools in any form is not allowed!"
+);
+console.log(
+  `Telegram: ${colors.green(
+    "https://t.me/power_codes"
+  )}  ___  Facebook: ${colors.blue("https://t.me/powercodes")}`
+);
+console.log(
+  `🚀 Update the latest tools at: 👉 ${colors.gray(
+    "https://github.com"
+  )} 👈`
+);
+console.log("");
+console.log("");
+
+server.checkVersion(VERSION);
+server.showNoti();
+console.log("");
+const users = await userService.loadUser();
+
+for (const [index, user] of users.entries()) {
+  countdownList.push({
+    running: true,
+    time: 480 * 60,
+    created: dayjs().unix(),
+  });
+  run(user, index);
+}
+
+if (IS_SHOW_COUNTDOWN && users.length) {
+  let isLog = false;
+  setInterval(async () => {
+    const isPauseAll = !countdownList.some((item) => item.running === true);
+
+    if (isPauseAll) {
+      await delayHelper.delay(1);
+      if (!isLog) {
+        console.log(
+          "========================================================================================="
+        );
+        isLog = true;
+      }
+      const minTimeCountdown = countdownList.reduce((minItem, currentItem) => {
+        
+        const currentOffset = dayjs().unix() - currentItem.created;
+        const minOffset = dayjs().unix() - minItem.created;
+        return currentItem.time - currentOffset < minItem.time - minOffset
+          ? currentItem
+          : minItem;
+      }, countdownList[0]);
+      const offset = dayjs().unix() - minTimeCountdown.created;
+      const countdown = minTimeCountdown.time - offset;
+      process.stdout.write("\x1b[K");
+      process.stdout.write(
+        colors.white(
+          `[${dayjs().format(
+            "DD-MM-YYYY HH:mm:ss"
+            )}] All threads have run, need to wait: ${colors.blue(
+            datetimeHelper.formatTime(countdown)
+          )}     \r`
+        )
+      );
+    } else {
+      isLog = false;
+    }
+  }, 1000);
+
+  process.on("SIGINT", () => {
+    console.log("");
+    process.stdout.write("\x1b[K");
+    process.exit(); 
+  });
+}
+
+setInterval(() => {}, 1000); 
